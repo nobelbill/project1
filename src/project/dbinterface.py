4e8337262bdae5c49c8094d238e7c6abbe36260c
nobelbill <nobelbill@gmail.com>
2015-02-16 09:53:50 +0900

    source refactor test case add

--- src/project/dbinterface.py
+++ src/project/dbinterface.py
@@ -5,21 +5,23 @@ import sqlite3
 sqlite_con =  None
 database = 'comic.db'
 cursor =  None
-last_number_sql = "select last from comic"
+last_number_sql = "select last from comit"
 
 
 def sqlite_connect() :
     try:
-        sqlite_con =  sqlite3.connect(database)
-
+        sqlite_con = sqlite3.connect(database)
+        print "sqlite connect"
+        return  sqlite_con
     except sqlite3.Error, e:
         print "Error %s:" % e.args[0]
         sys.exit(1)
 
-def sqlite_disconnect() :
+def sqlite_disconnect(con) :
     try:
-        if sqlite_con :
-            sqlite_con.close()
+        if con :
+            con.close()
+        print "sqlite disconnect"
     except sqlite3.Error ,e:
         print "disconnect error %s" % e.args[0]
         sys.exit(0)
@@ -27,15 +29,14 @@ def sqlite_disconnect() :
 
 def sqlite_select_last_number() :
     try:
-        sqlite_connect()
-        cursor = sqlite_con.cursor()
+        con = sqlite_connect()
+        cursor = con.cursor()
         cursor.execute(last_number_sql)
         last =  cursor.fetchone()
         print last
+        sqlite_disconnect(con)
     except sqlite3.Error , e:
         print "select error %s" % e.args[0]
 
 
-sqlite_connect()
 sqlite_select_last_number()
-sqlite_disconnect()
---
+++ src/project/test/test_dbinterface.py
@@ -0,0 +1,9 @@
+__author__ = '1002097'
+
+import unittest
+import project.dbinterface
+
+class test_dbinterface(unittest.TestCase) :
+
+    def test_sqlite_connect(self):
+        from dbinterface
